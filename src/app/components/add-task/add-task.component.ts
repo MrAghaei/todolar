6daa531162c16e24b347165ff64b5b9e2bfbc6f5
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-02-17 11:00:27 +0330

    [#6-ADD] UI service (#3)

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {Subscription} from "rxjs";
+import {UiService} from "../../services/Ui.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
-
+  showAddTask!: boolean;
+  subscription!: Subscription;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value)=> this.showAddTask = value)
+  }
 }
--- src/app/services/ui.service.spec.ts
+++ src/app/services/ui.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UiService } from './ui.service';
+import { UiService } from './Ui.service';
 
 describe('UiService', () => {
   let service: UiService;
