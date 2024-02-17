60fa645e6e06064d977e77891cebc27f18897825
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-02-17 10:19:18 +0330

    [#1-implement] mainpage component

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors:{
+      "main": "#087f5b",
+      "tea-rose": "#12b886",
+      "text": "#e6fcf5"
+    },
+    extend: {},
+  },
+
+  plugins: [],
+}
