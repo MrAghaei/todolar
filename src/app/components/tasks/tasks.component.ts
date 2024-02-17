2a2aff3e06f466497f4fe31f3765e6668010b930
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-02-17 11:24:17 +0330

    [#4-ADD] task component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import {AddTaskComponent} from "../add-task/add-task.component";
+import {TaskItemComponent} from "../task-item/task-item.component";
+import {Task} from "../../../Tasks";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    AddTaskComponent
+    AddTaskComponent,
+    TaskItemComponent
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
+
 export class TasksComponent {
 
+ tasks: Task[] = []
+
+  deleteTask(task: Task){
+    console.log(task);
+  }
+  addTask(task: Task){
+    console.log(task);
+  }
 }
