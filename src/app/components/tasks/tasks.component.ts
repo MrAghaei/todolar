13bc441402816837eec22ea16cc7cd8a55ac2031
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-02-17 11:14:33 +0330

    [#4-FIX] add-task parent

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {AddTaskComponent} from "../add-task/add-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [
+    AddTaskComponent
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
